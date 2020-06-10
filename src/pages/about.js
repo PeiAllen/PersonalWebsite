20b4fdc2525900f6cdcd45b41e9c6cdf42aaf5fb
Allen <peiall@utschools.ca>
2020-06-10 11:54:09 -0400

    Update Abouttext

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ export default function About() {
       <Navbar/>
       <div>
         <h1 style={{talign:'center'}}>Hi, I'm Allen Pei.</h1>
-        <body>lorum ipsum lmfao</body>
+        <body>Hi, I'm an avid Competitive Programmer and Mathematician with an interest in spreading to other branches of computer science. I work with passion and I hope to meet many new people and make many new friends.</body>
       </div>
     </Router>
   );
